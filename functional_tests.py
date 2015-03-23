b39511422461cbe86275708b8dea90fe3e6ba454
Alan Dershowitz <deyijia@126.com>
2015-03-23 23:31:42 +0800

    Functional test now checks we can input a to-do item.

--- functional_tests.py
+++ functional_tests.py
@@ -20,6 +20,10 @@
 from selenium import webdriver 
 # now a new change:import builin module:unitest
 import unittest
+
+# after commit 'can returns html',first add
+from selenium.webdriver.common.keys import Keys
+
 class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -30,7 +34,33 @@ class NewVisitorTest(unittest.TestCase):
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://54.199.137.210:8080')
-		self.assertIn('To-Do',self.browser.title)
+		# after commit 'can returns html',first add
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		#self.assertIn('To-Do',self.browser.title)
+
+		# after commit 'can returns html',first add
+		self.assertIn('To-Do',header_text)
+
+		# she is invited to enter a to-do lists 
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+				inputbox.get_attribute('placeholder'),'Enter a to-do item'
+		)
+
+		# she types 'buy peacock feathers' into a text box(Edith's hobby
+		# is tying fly-fishing lures)
+		inputbox.send_keys('Buy peacock feathers')
+		# when she hits enter,the page updates,and now the page lists
+		# "1:Buy peacock feathers" as an item in a to-do list table
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(any(row.text=='1:Buy peacock feathers' for row in rows))
+
+		# there is still a text box inviting her to add another item.
+		# she enters 'Use peacock feathers to make a fly'(Edith is very methodical)
+
 		self.fail('finish the test!')
 		# rest of comments.
 if __name__ == '__main__':
