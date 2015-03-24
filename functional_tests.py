d8247da0bc41af290fb738a675e375954dbdb4d4
Alan Dershowitz <deyijia@126.com>
2015-03-24 23:51:35 +0800

    FT refactor,use helper func:check_for_row_in_list_table()

--- functional_tests.py
+++ functional_tests.py
@@ -34,6 +34,12 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	# a helper method.
+	def check_for_row_in_list_table(self,row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text,[row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://54.199.137.210:8080')
 		# after commit 'can returns html',first add
@@ -55,23 +61,23 @@ class NewVisitorTest(unittest.TestCase):
 		# when she hits enter,the page updates,and now the page lists
 		# "1:Buy peacock feathers" as an item in a to-do list table
 		inputbox.send_keys(Keys.ENTER)
+
 		# add sleep time
 		#time.sleep(10)
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-	#	self.assertTrue(any(row.text=='1:Buy peacock feathers' for row in rows),'New to-do item did not appear in table -- its text was:\n%s' % (table.text,))
-		self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+
+		# refactor code (use helper method)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# there is still a text box inviting her to add another item.
 		# she enters 'Use peacock feathers to make a fly'(Edith is very methodical)
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-# The page updates again ,and now shows both items on her list.
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
+
+		# The page updates again ,and now shows both items on her list.
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
 		# Edith wonders whether the site will remember her list.then she sees 
 		# that the side has generated a unique url for her -- there
 		# is some explanatory text to that effect.
@@ -83,3 +89,6 @@ if __name__ == '__main__':
 	# original is "unittest.main(warnings='ignore')"
 	#unittest.main()
 	unittest.main(warnings='ignore')
+
+
+		
