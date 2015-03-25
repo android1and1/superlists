b981ffb61f604262d02e8d9a7d628322320407bf
Alan Dershowitz <deyijia@126.com>
2015-03-25 16:22:12 +0800

    Model for list Items and associated migration.

--- lists/tests.py
+++ lists/tests.py
@@ -10,6 +10,9 @@ from django.http import HttpRequest
 # 4th 
 from django.template.loader import render_to_string
 
+
+# 5th 
+from lists.models import Item
 # first FT content.
 #class SmokeTest(TestCase):
 #	def test_bad_maths(self):
@@ -50,3 +53,25 @@ class HomePageTest(TestCase):
 		)
 		
 		self.assertEqual(response.content.decode(),expected_html)
+
+
+# 5th
+class ItemModelTest(TestCase):
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first(ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		
+		saved_items =  Item.objects.all()
+		self.assertEqual(saved_items.count(),2)
+	
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text,'The first(ever) list item')
+		self.assertEqual(second_saved_item.text,'Item the second')
+
