3ee53f47a549796aea90f672254788708b98961d
ultrabook notitle <ultrabook@nothis.com>
2015-04-08 21:52:06 +0800

    Yup,we get to the same point we did before.That's a nicely self-contained' commit in ....PDF-P96-chapter6 Getting to the Minimum viable site.

--- lists/tests.py
+++ lists/tests.py
@@ -1,81 +1,24 @@
 from django.test import TestCase
-# second add
 from django.core.urlresolvers import resolve
 from lists.views import home_page
-
-# third add
 from django.http import HttpRequest 
-# Create your tests here.
-
-# 4th 
 from django.template.loader import render_to_string
-
-
-# 5th 
 from lists.models import Item
-# first FT content.
-#class SmokeTest(TestCase):
-#	def test_bad_maths(self):
-#		self.assertEqual(1 + 1 , 3)
-
 
 class HomePageTest(TestCase):
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')
-
 		self.assertEqual(found.func,home_page)
 
 	def test_home_page_returns_correct_html(self):
 		request =  HttpRequest()
 		response = home_page(request)
-# despresented for 'Dont test constant!'
-		#self.assertTrue(response.content.startswith(b'<html>'))
-		#self.assertIn(b'<title>To-Do lists</title>',response.content)
-		#self.assertTrue(response.content.endswith(b'</html>'))
-
 
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(),expected_html)
 
-
-# handle 'POST'
-	def test_home_page_can_save_a_POST_request(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = 'A new list item'
-
-		response = home_page(request)
-# 9th
-		self.assertEqual(Item.objects.count(),1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text,'A new list item')
-# end of 9th
-
-#		self.assertIn('A new list item',response.content.decode())
-		# 10th
-		# 11th
-		#self.assertEqual(response.status_code,302)
-		#self.assertEqual(response['location'],'/')
-
-		# 8th
-#		expected_html = render_to_string('home.html',
-#			{'new_item_text':'A new list item'}
-#		)
 		
-#		self.assertEqual(response.content.decode(),expected_html)
 
-#
-	def test_home_pag_redirects_after_POST(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = 'A new list item'
-
-		response = home_page(request)
-		
-		self.assertEqual(response.status_code,302)
-		self.assertEqual(response['location'],'/')
-		
-# 5th
 class ItemModelTest(TestCase):
 	def test_saving_and_retrieving_items(self):
 		first_item = Item()
@@ -85,7 +28,6 @@ class ItemModelTest(TestCase):
 		second_item = Item()
 		second_item.text = 'Item the second'
 		second_item.save()
-
 		
 		saved_items =  Item.objects.all()
 		self.assertEqual(saved_items.count(),2)
@@ -95,21 +37,42 @@ class ItemModelTest(TestCase):
 		self.assertEqual(first_saved_item.text,'The first(ever) list item')
 		self.assertEqual(second_saved_item.text,'Item the second')
 
-# 10th
-	def test_home_page_only_saves_items_when_necessary(self):
-		request = HttpRequest()
-		home_page(request)
-		self.assertEqual(Item.objects.count(),0)
 
+	#def test_home_page_only_saves_items_when_necessary(self):
+	#	request = HttpRequest()
+	#	home_page(request)
+	#	self.assertEqual(Item.objects.count(),0)
 
-	def test_home_page_displays_all_list_items(self):
+class ListViewTest(TestCase):
+	def test_uses_list_template(self):
+		response = self.client.get('/lists/the-only-list-in-the-world/')
+		self.assertTemplateUsed(response,'list.html')
+
+	def test_display_all_items(self):
 		Item.objects.create(text='itemey 1')
 		Item.objects.create(text='itemey 2')
+
+		response = self.client.get('/lists/the-only-list-in-the-world/')
+
+		self.assertContains(response,'itemey 1')
+		self.assertContains(response,'itemey 2')
+class NewListTest(TestCase):
+	def test_save_a_POST_request(self):
+		self.client.post(
+			'/lists/new',
+			data = {'item_text':'A new list item'}
+		)
+
+		self.assertEqual(Item.objects.count(),1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text,'A new list item')
+
+	def test_redirects_after_POST(self):
+		response = self.client.post(
+			'/lists/new',
+			data = {'item_text':'A new list item'}
+		)
 		
-		request = HttpRequest()
-		response = home_page(request)
-
-		self.assertIn('itemey 1',response.content.decode())
-		self.assertIn('itemey 2',response.content.decode())
-
-
+		#self.assertEqual(response.status_code,302)
+		#self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
+		self.assertRedirects(response,'/lists/the-only-list-in-the-world/')
--- lists/views.py
+++ lists/views.py
@@ -1,62 +1,14 @@
 from django.shortcuts import render
-
-# 10th
 from django.shortcuts import redirect
-
-# second add
-#from django.http import HttpResponse
-# Create your views here.
-#home_page=None
-
-
-#9th
 from lists.models import Item
 
 def home_page(request):
-	#pass
-	#second add
-#	return HttpResponse()
-	# third add
-	#return HttpResponse('<html>')
-	#4th 
-	#return HttpResponse('<html><title>To-Do lists</title>')
-	# 5th
-#	return HttpResponse('<html><title>To-Do lists</title></html>')
-	# 6th
-	#return render(request,'home.html')
-	# 7th
-	#if request.method == 'POST':
-	#	return HttpResponse(request.POST['item_text'])
-	# 8th
-	#return render(request,'home.html')
-#	return render(request,'home.html',{
-#		'new_item_text':request.POST.get('item_text',''),
-#		}
-#		)
-	# 9th
-#	item = Item()
-#	item.text = request.POST.get('item_text','')
-#	item.save()
-#	
-#	return render(request,'home.html',{
-#
-#			'new_item_text':request.POST.get('item_text','')
-#			}
-#
-#		)
-#	
-	#10
-	if request.method == 'POST':
-		new_item_text = request.POST['item_text']
-		Item.objects.create(text=new_item_text)
-		# 10th redirect url.
-		return redirect('/')
-	#else:
-		#new_item_text = ''
+	return render(request,'home.html')
+
+def view_list(request):
 	items = Item.objects.all()
-	return render(request,'home.html',{
-		#'new_item_text':new_item_text,
-		# 11th
-		'items':items,
-		}
-		)
+	return render(request,'list.html',{'items':items,})
+
+def new_list(request):
+	Item.objects.create(text=request.POST['item_text'])
+	return redirect('/lists/the-only-list-in-the-world/')
