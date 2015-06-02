d3c1d5c730b6f17939bd87ee67eec05c9a7a1831
ultrabook notitle <ultrabook@nothis.com>
2015-06-02 16:09:39 +0800

    the end of chapter11:A Simple Form,all is right.

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,12 +1,7 @@
 from django.test import TestCase
 from lists.forms import EMPTY_LIST_ERROR,ItemForm
-from unittest import skip
+from lists.models import Item,List
 class ItemFormTest(TestCase):
-	@skip
-	def test_form_renders_item_text_input(self):
-		form = ItemForm()
-		self.fail(form.as_p())
-
 	def test_form_item_has_placeholder_and_css_classes(self):
 		form = ItemForm()
 		self.assertIn('placeholder="Enter a to-do item"',form.as_p())
@@ -19,3 +14,11 @@ class ItemFormTest(TestCase):
 			form.errors['text'],
 			[EMPTY_LIST_ERROR]
 		)
+
+	def test_form_save_handles_saving_to_a_list(self):
+		list_ = List.objects.create()
+		form = ItemForm(data={'text':'do me'})
+		new_item = form.save(for_list=list_)
+		self.assertEqual(new_item,Item.objects.first())
+		self.assertEqual(new_item.text,'do me')
+		self.assertEqual(new_item.list,list_)
